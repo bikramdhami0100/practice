8b974ff0ca353bcb11e8951a807e4f34a85123bb
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-06-06 18:30:09 +0545

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,11 @@
-"use client"
-import { useEffect, useState, useRef } from 'react';
-import { io } from 'socket.io-client';
+import React from 'react'
 
-const Home = () => {
-  const [message, setMessage] = useState('');
-  const socketRef = useRef();
-
-  // useEffect(() => {
-  //   const socket = socketRef.current = io('http://localhost:3000/api/socket');
-
-  //   socket.on('connect', () => {
-  //     console.log('connected to server');
-  //   });
-
-  //   socket.on('message', (msg) => {
-  //     setMessage(msg);
-  //   });
-
-  //   return () => {
-  //     socket.disconnect();
-  //   };
-  // }, []);
-
+function Home() {
   return (
-    <div>
-      <h1>Socket.IO with Next.js</h1>
-      <div className=' hover:scale-150  h-[100px] w-[100px] bg-blue-600'>
-
-      </div>
-      {/* <p>Message from server: {message}</p> */}
+    <div className=' bg-black'>
+      Home
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
